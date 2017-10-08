ddd0f90de48675587919d1bdc184c1344871d0b2
yhao <hyppenny7@gmail.com>
2017-10-08 17:02:11 +0000

    ClassDefinition

--- server.py
+++ server.py
@@ -4,16 +4,26 @@ if (len(sys.argv) < 3):
     print("Server usage: python server.py IP PORT")
     sys.exit(0)
 
+PORT = sys.argv[2]
+PORT = int[PORT]
 
 
-class Chatroom():
-    pass
+class ChatRoom():
+    def __init__(self):
+        self.message = []
+        self.client = []
+
 
 class Server():
-    pass
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(("0.0.0.0", PORT))
+
 
 class ChatInfo():
-    pass
-
+    def __init__(self):
+        self.room = {}
+        self.roomRef = {}
 
 
