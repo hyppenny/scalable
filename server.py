5f1a572b48693b4a7ca083964cabfe1fe6933315
yhao <hyppenny7@gmail.com>
2017-10-08 16:05:45 +0000

    IninitClass

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket, sys
+
+if (len(sys.argv) < 3):
+    print("Server usage: python server.py IP PORT")
+    sys.exit(0)
+
+
+
+class Chatroom():
+    pass
+
+class Server():
+    pass
+
+class ChatInfo():
+    pass
+
+
+
