f13642a688b731c55804ec21cbf6c47261d4edf5
yhao <hyppenny7@gmail.com>
2017-10-10 15:10:23 +0000

    JoinMessage

--- server.py
+++ server.py
@@ -41,7 +41,27 @@ class Process():
             self.pool.kill()
         elif message.startswith("HELO "):
             self.connect.send("hello")
+        elif message.startswith("JOIN_CHATROOM: "):
+            roomName = message.splitlines()[0][len("JOIN_CHATROOM: "):]
+            clientName = message.splitlines()[3][len("CLIENT_NAME: "):]
+            self.pool.lockState.acquire()
 
+            clientID = self.pool.state.idCounter
+            self.pool.state.idCounter += 1
+
+            if roomName in self.pool.state.rooms:
+                roomRef = self.pool.state.rooms[roomName].ref
+            else:
+                roomRef = self.pool.state.refCounter
+                self.pool.state.rooms[roomName] = ChatRoom(roomRef)
+                self.pool.state.refCounter += 1
+                room = self.pool.state.rooms[roomName]
+
+            if (len(room.client) > 0):
+                joinMessage = "{0} has joined the chatroom".format(clientName)
+                room.messages.append([clientName, joinMessage, set(room.clients)])
+                self.pool.state.rooms[roomName].clients.append(clientID)
+                self.pool.lockState.release()
 
 class Pool():
     def __init__(self):
@@ -52,10 +72,13 @@ class Pool():
         self.state = ChatInfo()
         self.threadCounter = 0
         self.kill = False
+        self.connect = None
         for i in range(2):
             reply = "HELO"
             return reply
 
     def kill(self):
         self.kill = True
+
+
 
